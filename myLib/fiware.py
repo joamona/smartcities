53e221656f02872fad7c7fa7bcd6f613057955f7
joamona <joamona@cgf.upv.es>
2024-05-16 11:25:46 +0200

    m

--- myLib/fiware.py
+++ myLib/fiware.py
@@ -1,8 +1,8 @@
-'''
+"""
 Created on 2 jun 2023
 
 @author: joamona
-'''
+"""
 import os
 import json
 import inspect
@@ -20,9 +20,9 @@ class Fiware():
         #attributes
         self.url=url
         self.user=user
-        self.entities = '/v2/entities'
+        self.entities = "/v2/entities"
         self.urlEntities=self.url + self.entities
-        self.headers={'Content-Type': 'application/json'}
+        self.headers={"Content-Type": "application/json"}
         self.requesResult = None
         self.printInfo=printInfo
 
@@ -31,7 +31,7 @@ class Fiware():
 
     def printRequestResult(self):
         if self.printInfo:
-            print('url: ' + self.url)
+            print("url: " + self.url)
             print("Status code: " + str(self.requesResult.status_code))
             print("Text message: " + self.requesResult.text)
         
@@ -50,12 +50,13 @@ class Fiware():
         self.printDict(d=js.json())
 
     def getVersion(self):
-        url=self.url + '/version'
+        url=self.url + "/version"
+        print(f"Url: {url}")
         res = requests.get(url)
         return json.dumps(res.json(), indent=4)
     
     def createUrn(self,etype,ename):
-        urn = f'urn:ngsi-ld:{self.user}:{etype}:{ename}'
+        urn = f"urn:ngsi-ld:{self.user}:{etype}:{ename}"
         return urn
     
     def createEntity(self,etype,ename, attributes={}):
@@ -73,20 +74,20 @@ class Fiware():
         }
         """
         payload={
-            'id': self.createUrn(etype, ename),
-            'type':etype,
-            'name':{
-                'type':'Text',
-                'value':ename
+            "id": self.createUrn(etype, ename),
+            "type":etype,
+            "name":{
+                "type":"Text",
+                "value":ename
                 }
             }
         for key, value in attributes.items():
             payload[key]=value
             
         entity={
-            'type':etype,
-            'name':ename,
-            'payload':payload
+            "type":etype,
+            "name":ename,
+            "payload":payload
             }
         print(json.dumps(entity, indent=4))
         return entity
@@ -95,29 +96,29 @@ class Fiware():
         #coordinates=[longitude, latitude]
         
         payload={
-            'id':self.createUrn(etype, ename),
-            'type':etype,
-            'name':{
-                'type':'text',
-                'value':ename
+            "id":self.createUrn(etype, ename),
+            "type":etype,
+            "name":{
+                "type":"text",
+                "value":ename
                 },
-            'location':{
-                'type':'geo:json',
-                'value':{
-                    'type':'Point',
-                    'coordinates':coordinates
+            "location":{
+                "type":"geo:json",
+                "value":{
+                    "type":"Point",
+                    "coordinates":coordinates
                     }
                 }
             }
-        print('attributes',attributes)
+        print("attributes",attributes)
         for key, value in attributes.items():
             payload[key]=value
-        print('payload')
+        print("payload")
         print(payload)
         entity={
-            'type':etype,
-            'name':ename,
-            'payload':payload
+            "type":etype,
+            "name":ename,
+            "payload":payload
             }
         self.printDict(d=entity)
         return entity
@@ -126,11 +127,11 @@ class Fiware():
         """
         Fíjate que lo que se sube e el payload del diccionario
         """
-        print('Upload entity')
+        print("Upload entity")
         self.requesResult=requests.post(
             self.urlEntities,
             headers=self.headers,
-            data=json.dumps(entity['payload'])
+            data=json.dumps(entity["payload"])
             )
         fa=FiwareAnswer(answer=self.requesResult,entity=entity)
         return fa
@@ -139,27 +140,27 @@ class Fiware():
         lStatus=[]
         n=len(l)
         for i in range(len(l)):
-            print(f'Uploading entity: {i} of {n}')
+            print(f"Uploading entity: {i} of {n}")
             fa=self.uploadEntity(l[i])
             lStatus.append(fa)
         return lStatus
     
     def filterByType(self, etype:str, limit=1000):
-        self.url=self.urlEntities + '?type=' + etype + '&limit=' + str(limit)  + '&options=count'
+        self.url=self.urlEntities + "?type=" + etype + "&limit=" + str(limit)  + "&options=count"
         self.requesResult=requests.get(self.url)
         fa:FiwareAnswer=FiwareAnswer(answer=self.requesResult)
         return fa 
 
     def filterByIdPattern(self, idSubString:str, limit=1000):
-        '^.*Test.*$'
-        self.url=self.urlEntities + '?idPattern=^.*' + idSubString + '.*$' + '&limit=' + str(limit)  + '&options=count'
+        "^.*Test.*$"
+        self.url=self.urlEntities + "?idPattern=^.*" + idSubString + ".*$" + "&limit=" + str(limit)  + "&options=count"
         self.requesResult=requests.get(self.url)
         fa:FiwareAnswer=FiwareAnswer(answer=self.requesResult)
         return fa
 
     def filterByIdPatternAndType(self, idSubString:str,etype:str, limit=1000):
-        '^.*Test.*$'
-        self.url=self.urlEntities + '?idPattern=^.*' + idSubString + '.*$' + '&type=' + etype +'&limit=' + str(limit)  + '&options=count'
+        "^.*Test.*$"
+        self.url=self.urlEntities + "?idPattern=^.*" + idSubString + ".*$" + "&type=" + etype +"&limit=" + str(limit)  + "&options=count"
         self.requesResult=requests.get(self.url)
         fa:FiwareAnswer=FiwareAnswer(answer=self.requesResult)
         return fa
@@ -172,17 +173,17 @@ class Fiware():
         and join them in one result
         """
         parameters = {
-            'idPattern': self.user,
-            'offset': 0,
+            "idPattern": self.user,
+            "offset": 0,
         }
-        parameters['limit'] = limit
+        parameters["limit"] = limit
 
         if etype is not None:
-            parameters['type'] = etype
+            parameters["type"] = etype
 
         if fields is not None and isinstance(fields, dict):
             for key, value in fields.items():
-                parameters['q'] = f'{key}=={value}'
+                parameters["q"] = f"{key}=={value}"
         print("filterByUserAndProperties. Current parameters:")
         print(parameters.items())
         
@@ -195,15 +196,15 @@ class Fiware():
                 break
             entities += response.json()
             page += 1
-            parameters['offset'] = page * limit
+            parameters["offset"] = page * limit
         fa=FiwareAnswer(answer=response)
         fa.setResultingEntities(resultingEntities=entities)
         return fa
     """
-    def filterByAttributeValue(self, attributeName:str, attributeValue, operator='==', limit=1000):
+    def filterByAttributeValue(self, attributeName:str, attributeValue, operator="==", limit=1000):
         #Operator can be >, ==, <
         
-        self.url=self.urlEntities + '?q=' + attributeName + operator + str(attributeValue) + '&limit=' + str(limit) + '&options=count'
+        self.url=self.urlEntities + "?q=" + attributeName + operator + str(attributeValue) + "&limit=" + str(limit) + "&options=count"
         self.requesResult=requests.get(self.url)
         fa:FiwareAnswer=FiwareAnswer(answer=self.requesResult)
         return fa
@@ -211,18 +212,18 @@ class Fiware():
     
     def filterBySeveralAttributeValueConditions(self, andConditions:str, limit=1000):
         """
-        conditions must be like: 'accuracy>2;accuracy<5;mode==8'
+        conditions must be like: "accuracy>2;accuracy<5;mode==8"
         Means accuracy > 2 and accuracy <5 and mode ==8
         There is not OR condition.
         """
-        self.url=self.urlEntities + '?q=' + andConditions + '&limit=' + str(limit) + '&options=count'
+        self.url=self.urlEntities + "?q=" + andConditions + "&limit=" + str(limit) + "&options=count"
         self.requesResult=requests.get(self.url)
         fa:FiwareAnswer=FiwareAnswer( answer=self.requesResult)
         return fa
     """
     #this does not retrieve more than 1000 entities          
     def getAllEntities(self, limit=1000):
-        url=self.urlEntities + '?limit=' + str(limit) + '&options=count'
+        url=self.urlEntities + "?limit=" + str(limit) + "&options=count"
         fa = FiwareAnswer(requests.get(url)) 
         return fa
     """    
@@ -238,15 +239,15 @@ class Fiware():
     """
     
     def getEntityById(self,entity_id):
-        url=self.urlEntities + '/' + entity_id
+        url=self.urlEntities + "/" + entity_id
         self.requesResult=requests.get(url)
         return FiwareAnswer(answer=self.requesResult)
 
     def countEntities(self):
-        parameters={'options':'count'}
+        parameters={"options":"count"}
         r=requests.get(self.urlEntities,params=parameters)
         try:
-            count = int(r.headers['Fiware-Total-Count'])
+            count = int(r.headers["Fiware-Total-Count"])
         except:
             count = None
         return count
@@ -255,12 +256,12 @@ class Fiware():
         entity_id = self.createEntity(etype, ename)
         print(entity_id)
         if self.user not in entity_id:
-            print('You are not allowed to delete other user entities')
+            print("You are not allowed to delete other user entities")
             return 
-        url = self.urlEntities + '/' + entity_id
+        url = self.urlEntities + "/" + entity_id
         self.requesResult=requests.delete(url)
         self.printRequestResult()
-        return f'[{self.requesResult.status_code}]'
+        return f"[{self.requesResult.status_code}]"
     
     def deleteMyEntityById(self,entity_id):
         """
@@ -268,9 +269,9 @@ class Fiware():
         It is necessary select them and remove them one by one
         """
         if self.user not in entity_id:
-            print('You are not allowed to delete other user entities')
+            print("You are not allowed to delete other user entities")
             return 
-        url = self.urlEntities + '/' + entity_id
+        url = self.urlEntities + "/" + entity_id
         self.requesResult=requests.delete(url)
         fa:FiwareAnswer=FiwareAnswer(self.requesResult)
         return fa
@@ -281,21 +282,21 @@ class Fiware():
         n=len(fa1.resultingEntities)
         results:[FiwareAnswer]=[]
         for i in range(n):
-            print(f'Deleting entity {i+1} of {n}')
+            print(f"Deleting entity {i+1} of {n}")
             en=fa1.resultingEntities[i]
             #pay attention. Here the payload field is not
-            #when you upload an entity you get the id as en['payload']['id']
-            #but when you download an entity you simply use en['id']
-            fa:FiwareAnswer=self.deleteMyEntityById(en['id'])
+            #when you upload an entity you get the id as en["payload"]["id"]
+            #but when you download an entity you simply use en["id"]
+            fa:FiwareAnswer=self.deleteMyEntityById(en["id"])
             fa.entity=en
             results.append(fa)
 
     def createCsvEntities(self, etype, csvData:list, csvHeader:list):
         #csvData es una lista de listas 
-        #csv header is: ['PID', 'LONGITUDE', 'LATITUDE', 'ACCURACY', 'DATE']
+        #csv header is: ["PID", "LONGITUDE", "LATITUDE", "ACCURACY", "DATE"]
         #ename es PID
         
-        specialFields=['PID', 'LONGITUDE', 'LATITUDE']
+        specialFields=["PID", "LONGITUDE", "LATITUDE"]
         
         csvEntities=[]
         for record in csvData:
@@ -305,18 +306,18 @@ class Fiware():
                     value=record[index]
                     if isinstance(value, float):
                         attributes[fieldName.lower()]={
-                            'type': 'Float',
-                            'value': value
+                            "type": "Float",
+                            "value": value
                             }
                     elif isinstance(value, int):
                         attributes[fieldName.lower()]={
-                            'type': 'Integer',
-                            'value': value
+                            "type": "Integer",
+                            "value": value
                             }
                     else:
                         attributes[fieldName.lower()]={
-                            'type': 'Text',
-                            'value': value
+                            "type": "Text",
+                            "value": value
                             }   
             geoEntity=self.createGeoEntity(etype=etype, 
                                            ename=record[0], 
